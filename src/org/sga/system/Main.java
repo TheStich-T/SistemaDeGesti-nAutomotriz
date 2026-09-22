264e1556204a439f4509681c0b65f84490b69e92
Antony Perez <aperez-2023350@kinal.edu.gt>
2026-09-21 20:07:49 -0600

    feat(main) - Se creo el proyectyo de NeatBeans y la configuracion de JavaFX on la estrucura del proyecto

---
+++ src/org/sga/system/Main.java
@@ -0,0 +1,21 @@
+package org.sga.system;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application { 
+    @Override
+    public void start(Stage escenarioPrincipal) throws Exception {
+        VBox raiz = new VBox();
+        Scene escena = new Scene(raiz);
+        
+        escenarioPrincipal.setTitle("Sistema De Gestión Automotriz (SGA)");
+        escenarioPrincipal.setScene(escena);
+        escenarioPrincipal.show();
+    }
+         public static void Main (String[] args) {
+    launch(args);
+     }
+}
