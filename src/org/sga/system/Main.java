50d295c486c89a8190765769d13e43d5486018c3
Antony Perez <aperez-2023350@kinal.edu.gt>
2026-09-24 09:29:52 -0600

    fix(Main) - Se agrego el cambio de escena para ver el login - Sprint 1

--- src/org/sga/system/Main.java
+++ src/org/sga/system/Main.java
@@ -1,21 +1,38 @@
 package org.sga.system;
 
+import java.io.IOException;
+import java.net.URL;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class Main extends Application { 
-    @Override
-    public void start(Stage escenarioPrincipal) throws Exception {
-        VBox raiz = new VBox();
-        Scene escena = new Scene(raiz);
-        
-        escenarioPrincipal.setTitle("Sistema De Gestión Automotriz (SGA)");
-        escenarioPrincipal.setScene(escena);
+public class Main extends Application {
+
+    private static final String LOGIN_VIEW = "/org/sga/view/LoginView.fxml";
+    private static Stage escenarioPrincipal;
+
+    public static void cambiarEscena(String rutaFXML) throws IOException {
+        URL recurso = Main.class.getResource(rutaFXML);
+        if (recurso == null) {
+            throw new IOException("No se encontró la vista: " + rutaFXML);
+        }
+        Parent raiz = FXMLLoader.load(recurso);
+        escenarioPrincipal.setScene(new Scene(raiz));
+        escenarioPrincipal.sizeToScene();
+        escenarioPrincipal.centerOnScreen();
         escenarioPrincipal.show();
     }
-         public static void main (String[] args) {
+
+    @Override
+    public void start(Stage escenario) throws Exception {
+        Main.escenarioPrincipal = escenario;
+        escenario.setTitle("Sistema De Gestión Automotriz (SGA)");
+        cambiarEscena(LOGIN_VIEW);
+    }
+
+    public static void main(String[] args) {
         launch(args);
-     }
+    }
 }
